3d0dc8482493e434110791caa4dc4bf400cef02c
Ajongbah Malvin <malvinajongbah@gmail.com>
2020-02-23 11:56:19 +0000

    UI done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import TaskList from './components/TaskList'
+import TaskContextProvider from './context/TaskListContext';
+import TaskForm from './components/TaskForm';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Task Tracker
-    </div>
+    <TaskContextProvider>
+      <div className='container'>
+        <div className="app-wrapper">
+          <Header />
+          <div className="main">
+            <TaskForm />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </TaskContextProvider>
   );
 }
 
